a6a782d5fcd840aa304d338c76965363c67e0ead
SubtlePhenakism <subtlephenakism@gmail.com>
2015-05-03 18:39:34 -0700

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,73 @@
+// Brunchball - Events with friends
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'brunchball' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'brunchball.services' is found in services.js
+// 'brunchball.controllers' is found in controllers.js
+angular.module('brunchball', ['ionic', 'brunchball.controllers', 'brunchball.services', 'firebase'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "templates/tabs.html"
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+      url: '/dash',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
+        }
+      }
+    })
+
+    .state('tab.friends', {
+      url: '/friends',
+      views: {
+        'tab-friends': {
+          templateUrl: 'templates/tab-friends.html',
+          controller: 'FriendsCtrl'
+        }
+      }
+    })
+
+    .state('tab.account', {
+      url: '/account',
+      views: {
+        'tab-account': {
+          templateUrl: 'templates/tab-account.html',
+          controller: 'AccountCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+});
---
+++ www/js/services.js
@@ -0,0 +1,20 @@
+angular.module('brunchball.services', [])
+/**
+ * A simple example service that returns some data.
+ */
+.factory('fireBaseData', function($firebase) {
+  var ref = new Firebase("https://brunchball.firebaseio.com/"),
+      refExpenses = new Firebase("https://brunchball.firebaseio.com/expenses"),
+      refRoomMates = new Firebase("https://brunchball.firebaseio.com/room-mates");
+  return {
+    ref: function () {
+      return ref;
+    },
+    refExpenses: function () {
+      return refExpenses;
+    },
+    refRoomMates: function () {
+      return refRoomMates;
+    }
+  }
+});
